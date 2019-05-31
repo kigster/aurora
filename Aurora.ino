574f949386bc1babef963070b843ff534ed4fd50
Konstantin Gredeskoul <kig@flowinc.com>
2019-05-31 02:11:54 -0700

    Adding my hardware

--- Aurora.ino
+++ Aurora.ino
@@ -6,7 +6,7 @@
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
+ * the Sy7oftware without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
@@ -24,13 +24,13 @@
 
 #include "Hardware.h"
 
-#include <SmartMatrix3.h>
+#include <SmartLEDShieldV4.h>
 
 #define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
 const uint8_t kMatrixWidth = 32;        // known working: 32, 64, 96, 128
 const uint8_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
-const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
-const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
+const uint8_t kRefreshDepth = 24;       // known working: 24, 36, 48
+const uint8_t kDmaBufferRows = 2;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
 const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
 const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
 const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
@@ -84,11 +84,11 @@ char versionText [] = "v1.7";
 elapsedMillis sinceStatusLedToggled;
 boolean statusLedState = false;
 
-bool sdAvailable = false;
+bool sdAvailable = true;
 IRrecv irReceiver(IR_RECV_PIN);
 
 boolean hasDS1307RTC = false;
-boolean hasTeensyRTC = false;
+boolean hasTeensyRTC = true;
 
 rotationDegrees rotation = rotation0;
 
@@ -176,7 +176,7 @@ GifPlayer gifPlayer;
 #include "Geometry.h"
 
 tmElements_t time;
-bool isTimeAvailable = false;
+bool isTimeAvailable = true;
 
 #include "ClockDigitalShort.h"
 ClockDigitalShort clockDigitalShort;
